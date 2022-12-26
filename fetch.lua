8da8c60c0ad1730b415faabf13557f7db4b59bd1
Alteral323 <62857494+Alteral323@users.noreply.github.com>
2022-12-26 16:37:53 -0500

    Update fetch.lua

--- fetch.lua
+++ fetch.lua
@@ -1,27 +1,27 @@
 local list = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://raw.githubusercontent.com/Alteral323/w/main/list.json", true))
 local sha = loadstring(game:HttpGet("https://raw.githubusercontent.com/Alteral323/w/main/sha.lua"))()
 local cache = {}
-local findtable = function(tab, obj)
+local find = function(tab, obj)
     for _, v in pairs(tab) do
-        if v == obj or type(v) == "table" and v.hash == obj then
+        if v == obj or type(v) == "table" and table.find(v, obj) then
             return v
         end
     end
     return nil
 end
-local vec = function(str)
+local create = function(str)
     return sha.sha512(tostring(str) .. "VC")
 end
 local hash = function(str)
     str = tostring(str)
     if cache[str] == nil then
-        cache[str] = vec(str)
+        cache[str] = create(str)
     end
     return cache[str] or ""
 end
 return {
     check = function(plr)
-        return findtable(list, hash(plr.UserId))
+        return find(list, hash(plr.UserId))
     end,
-    make = vec
+    make = create
 }
